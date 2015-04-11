7f61450b57f98a69fea7222fd32c28322ac9fef5
kamhix <kimly02@gmail.com>
2015-04-11 15:40:38 +0100

    Fix a bug in message service

--- server/services/message.js
+++ server/services/message.js
@@ -2,7 +2,7 @@
 
 var telerivet = require('telerivet');
 
-exports.send = function (content, to_number, phone_id,, callback) {
+exports.send = function (content, to_number, phone_id, callback) {
   var tr = new telerivet.API(process.env.TELERIVET_API);
   var project = tr.initProjectById(process.env.TELERIVET_PROJECT);
 
