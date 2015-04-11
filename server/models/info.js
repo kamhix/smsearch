aa47bb920dfb13418d00792cd81f752b7eb08596
Armand ACCROMBESSI <armand.accrombessi@tekxl.com>
2015-04-11 17:36:12 +0100

    Create an index on the tags field

--- server/models/info.js
+++ server/models/info.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 var InfoSchema = new mongoose.Schema({
   info: {type: String},
-  tags: {type: String}
+  tags: {type: [String], index: true}
 });
 
 module.exports = mongoose.model('Info', InfoSchema);
