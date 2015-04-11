86f5365973ddc36abc00fa71a1650cde344339f4
Armand ACCROMBESSI <armand.accrombessi@tekxl.com>
2015-04-11 18:06:22 +0100

    Add a search method to Infos service

--- server/services/info.js
+++ server/services/info.js
@@ -11,3 +11,18 @@ exports.add = function (info, tags, callback) {
 
   newInfo.save(callback);
 };
+
+exports.search = function (message, callback) {
+  message = message.toLowerCase();
+  message = message.split(' ');
+
+  if(message.length > 0) {
+    var query = Info.find({ $text: { $search: message[0] } });
+  }
+
+  for (var i = 1, n = message.length; i < n; i++) {
+    query = query.find({ $text: { $search: message[i] } });
+  };
+
+  query.exec(callback);
+};
